8ad4261b26c12bfc114dcbf210366ff38ad4d574
ShiroTsuma-github <spam.shiro@gmail.com>
2023-11-02 19:19:09 +0100

    podstawowa struktura projektu

---
+++ packages/test.py
@@ -0,0 +1,12 @@
+from packages import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
